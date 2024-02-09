827615080ac81848bf3cb062748c1062efb61221
cfoo <congfoo@gmail.com>
2024-02-08 21:32:13 -0800

    Autocommit from  on 2024-02-08 21:32:13
    Klipper on commit: 96ab906
    
    Moonraker on commit: 3008a13
    
    Mainsail version: v2.9.1

--- bin/autocommit.sh
+++ bin/autocommit.sh
@@ -72,7 +72,7 @@ push_config(){
   git pull origin master
   git add .
   current_date=$(date +"%Y-%m-%d %T")
-  git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+  git commit -m "Autocommit from $HOSTNAME on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
   git push origin master
 }
 
